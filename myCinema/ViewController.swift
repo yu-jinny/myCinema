34195e13aae5f8c87cb13841f34a5655ad8c9983
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-01-17 11:46:00 +0900

    Initial Commit

---
+++ myCinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myCinema
+//
+//  Created by t2023-m0028 on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
